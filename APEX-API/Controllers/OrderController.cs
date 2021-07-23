6f478bb3743c5ec73c1689b4be5442f18c8b686d
Detective-K <d02044@apexdyna.com>
2021-07-23 15:11:06 +0800

    first commit

---
+++ APEX-API/Controllers/OrderController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace APEX_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        // GET: api/<OrderController>
+        [Route("")]
+        [Route("Home")]
+        [Route("[controller]/[action]")]
+        public string Index()
+        {
+            return Convert.ToString("yes");
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<OrderController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return Convert.ToString(id);
+        }
+
+        // GET api/<OrderController>/5
+        [HttpGet ("[action]")]
+        public string Get2()
+        {
+            return Convert.ToString("get2");
+        }
+
+        // POST api/<OrderController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<OrderController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<OrderController>/5
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return Convert.ToString(id);
+        }
+    }
+
+}
